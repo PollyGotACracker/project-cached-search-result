04bd287ce3cd9eabb75f867df692e03995decb24
PollyGotACracker <bitterns96@gmail.com>
2023-07-19 20:03:29 +0900

    refactor: 데이터 캐싱 refactoring

--- src/hooks/useCacheData.tsx
+++ src/hooks/useCacheData.tsx
@@ -7,15 +7,12 @@ type CachedList<T> = { [key: string]: CachedItem<T> };
 const useCacheData = <T,>(expiresHour: number) => {
   const [cachedList, setCachedList] = useState<CachedList<T>>({});
 
-  const getCachedItem = (key: string): T[] | boolean => {
-    const result = cachedList[key];
-    return result ? result.data : false;
-  };
+  const getCachedItem = (key: string): T[] | boolean =>
+    cachedList.hasOwnProperty(key) ? cachedList[key].data : false;
 
   const setCachedItem = (key: string, value: T[] | []) => {
-    if (checkEmptyText(key)) return;
-    const currentTime = new Date().getTime();
-    const expires = new Date(currentTime + expiresHour * 60 * 60 * 1000);
+    if (checkEmptyText(key) || cachedList.hasOwnProperty(key)) return;
+    const expires = new Date(Date.now() + expiresHour * 60 * 60 * 1000);
     const cached = { data: value, expires: expires };
     setCachedList({ ...cachedList, [key]: cached });
   };
@@ -25,14 +22,14 @@ const useCacheData = <T,>(expiresHour: number) => {
       const updatedCachedList: CachedList<T> = cachedList;
       const currentTime = new Date();
 
-      for (const key in cachedList) {
+      for (const key in updatedCachedList) {
         const cachedItem = cachedList[key];
         if (cachedItem.expires <= currentTime) {
           delete updatedCachedList[key];
         }
       }
       setCachedList({ ...updatedCachedList });
-    }, 60 * 60 * 1000);
+    }, 60 * 1000);
 
     return () => clearInterval(interval);
   }, [cachedList]);
